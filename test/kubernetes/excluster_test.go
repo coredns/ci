d4c5b022f5fff546c42a0f39d90c8efb5323f2ed
Chris O'Haver <cohaver@infoblox.com>
2018-01-29 15:09:34 -0500

    add secure fallthrough test

--- test/kubernetes/excluster_test.go
+++ test/kubernetes/excluster_test.go
@@ -80,3 +80,34 @@ func TestKubernetesAPIFallthrough(t *testing.T) {
 		test.SortAndCheck(t, res, tc)
 	}
 }
+
+func TestKubernetesSecureAPIFallthrough(t *testing.T) {
+
+	corefile :=
+		`.:0 {
+    kubernetes cluster.local {
+        endpoint http://nonexistance:8443 http://invalidip:8443 https://127.0.0.1:8443
+        tls /root/.minikube/client.crt /root/.minikube/client.key /root/.minikube/ca.crt 
+    }`
+
+	server, udp, _, err := intTest.CoreDNSServerAndPorts(corefile)
+	if err != nil {
+		t.Fatalf("Could not get CoreDNS serving instance: %s", err)
+	}
+	defer server.Stop()
+
+	// Work-around for timing condition that results in no-data being returned in test environment.
+	time.Sleep(3 * time.Second)
+
+	for _, tc := range tests {
+
+		c := new(dns.Client)
+		m := tc.Msg()
+
+		res, _, err := c.Exchange(m, udp)
+		if err != nil {
+			t.Fatalf("Could not send query: %s", err)
+		}
+		test.SortAndCheck(t, res, tc)
+	}
+}
