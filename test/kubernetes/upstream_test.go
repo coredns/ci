892c3a1f844fa18efbc1881734df8b7315ba93a3
Chris O'Haver <cohaver@infoblox.com>
2018-02-09 13:50:49 -0500

    fix upstream self test

--- test/kubernetes/upstream_test.go
+++ test/kubernetes/upstream_test.go
@@ -5,10 +5,8 @@ import (
 	"testing"
 
 	"github.com/coredns/coredns/plugin/test"
-	intTest "github.com/coredns/coredns/test"
 
 	"github.com/miekg/dns"
-	"os"
 )
 
 var upstreamCases = []test.Case{
@@ -16,31 +14,25 @@ var upstreamCases = []test.Case{
 		Qname: "ext-svc.test-1.svc.cluster.local.", Qtype: dns.TypeA,
 		Rcode: dns.RcodeSuccess,
 		Answer: []dns.RR{
-			test.CNAME("ext-svc.test-1.svc.cluster.local.  303    IN      CNAME   example.net."),
 			test.A("example.net.                     303    IN      A       13.14.15.16"),
+			test.CNAME("ext-svc.test-1.svc.cluster.local.  303    IN      CNAME   example.net."),
 		},
 	},
 }
 
 func TestUpstreamToSelf(t *testing.T) {
-	upfile, rmFunc, err := intTest.TempFile(os.TempDir(), ExampleNet)
-	defer rmFunc()
-	if err != nil {
-		t.Fatalf("could not create file for CNAME upstream lookups: %s", err)
-	}
-
 	corefile := `    .:53 {
         errors
         log
-        kubernetes cluster.local 10.in-addr.arpa {
-            namespaces test-1
+        kubernetes cluster.local {
             upstream
         }
-		file ` + upfile + ` example.net
+        file /etc/coredns/Zonefile example.net
     }
 `
 
-	err = LoadCorefile(corefile)
+	println(corefile)
+	err := LoadCorefileAndZonefile(corefile, ExampleNet)
 	if err != nil {
 		t.Fatalf("Could not load corefile: %s", err)
 	}
@@ -73,8 +65,7 @@ func TestUpstreamToOther(t *testing.T) {
 	corefile := `    .:53 {
         errors
         log
-        kubernetes cluster.local 10.in-addr.arpa {
-            namespaces test-1
+        kubernetes cluster.local {
             upstream ` + udp + `
         }
     }
