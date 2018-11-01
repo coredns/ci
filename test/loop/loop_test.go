c1a9c07bfc0c6d9b4cf4e2901b42819c374f2a2f
Chris O'Haver <cohaver@infoblox.com>
2018-11-01 14:35:17 -0400

    give time for loop detection

--- test/loop/loop_test.go
+++ test/loop/loop_test.go
@@ -1,12 +1,14 @@
 package loop
 
 import (
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/coredns/ci/test/kubernetes"
-	"strings"
 )
 
+// TestLoopDetected verifies that forwarding to self will be detected as a loop
 func TestLoopDetected(t *testing.T) {
 
 	corefile := `    .:53 {
@@ -17,14 +19,14 @@ func TestLoopDetected(t *testing.T) {
     }
 `
 
-    err := kubernetes.LoadCorefile(corefile)
+	err := kubernetes.LoadCorefile(corefile)
 	if err != nil && !strings.Contains(err.Error(), "loop detected") {
 		t.Fatalf("Expected to see a \"loop detected\" error, got %s", err)
 	}
 }
 
+// TestLoopBlackHoleUpstream verifies that a dead upstream will not cause loop to falsely detect a loop
 func TestLoopBlackHoleUpstream(t *testing.T) {
-
 	// 240.0.0.0 is a "reserved for future use" range that routers apparently drop.
 	corefile := `    .:53 {
         errors
@@ -37,5 +39,10 @@ func TestLoopBlackHoleUpstream(t *testing.T) {
 	err := kubernetes.LoadCorefile(corefile)
 	if err != nil {
 		t.Fatalf("Could not load corefile: %s", err)
+	}
+	time.Sleep(time.Second * 10)
+	logs := kubernetes.CorednsLogs()
+	if strings.Contains(logs, "loop detected") {
+		t.Fatalf("Did not expect to see \"loop detected\" in logs, got %s", logs)
 	}
 }
