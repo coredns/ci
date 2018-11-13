3c3dd6a007ce7319897d6c887cc8c65cbd864497
Chris O'Haver <cohaver@infoblox.com>
2018-11-13 11:21:13 -0500

    fix failed log output

--- test/kubernetes/tools.go
+++ test/kubernetes/tools.go
@@ -243,7 +243,7 @@ func WaitNReady(maxWait, n int) error {
 
 // CorednsLogs returns the current coredns log
 func CorednsLogs() string {
-	name, _ := Kubectl("-n kube-system get pods -l k8s-app=kube-dns | grep Running | cut -f1 -d' ' | tr -d '\n'")
+	name, _ := Kubectl("-n kube-system get pods -l k8s-app=kube-dns | grep coredns | cut -f1 -d' ' | tr -d '\n'")
 	logs, _ := Kubectl("-n kube-system logs " + name)
 	return (logs)
 }
