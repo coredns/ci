42a47816794ec6b627591499460482e80c8167c0
Sandeep Rajan <srajan@infoblox.com>
2019-03-07 11:03:33 -0500

    refine test

--- test/reload/reload_test.go
+++ test/reload/reload_test.go
@@ -21,6 +21,22 @@ func TestReload(t *testing.T) {
 		t.Fatalf("Could not load corefile: %s", err)
 	}
 
+	err = kubernetes.StartClientPod("default")
+	if err != nil {
+		t.Fatalf("failed to start client pod: %s", err)
+	}
+
+	digCmd := "dig mytest.com +short"
+	cmdOut, err := kubernetes.Kubectl("-n default exec coredns-test-client -- " + digCmd)
+	if err != nil {
+		t.Fatalf("failed to execute query, got error: %s", err)
+	}
+
+	if !strings.EqualFold(cmdOut, "10.0.1.2") {
+		t.Fatalf("coredns failed to load the Configmap")
+	}
+
+	// Patch the CoreDNS ConfigMap with a new Corefile.
 	corefilePatchData := `patch configmap/coredns -n kube-system -p '{"data":{"Corefile":".:53 {\n       health\n       reload 2s\n       hosts {\n           10.0.1.20 mytest.com\n        }\n    }"}}'`
 	_, err = kubernetes.Kubectl(corefilePatchData)
 	if err != nil {
@@ -35,17 +51,11 @@ func TestReload(t *testing.T) {
 	}
 
 	// check if CoreDNS is reloading only once.
-	if strings.Count(logged, "[INFO] Reloading\n") >= 2 {
+	if strings.Count(logged, "[INFO] Reloading\n") > 1 {
 		t.Fatalf("coredns tried to reload more than once")
 	}
 
-	err = kubernetes.StartClientPod("default")
-	if err != nil {
-		t.Fatalf("failed to start client pod: %s", err)
-	}
-
-	digCmd := "dig mytest.com +short"
-	cmdOut, err := kubernetes.Kubectl("-n default exec coredns-test-client -- " + digCmd)
+	cmdOut, err = kubernetes.Kubectl("-n default exec coredns-test-client -- " + digCmd)
 	if err != nil {
 		t.Fatalf("failed to execute query, got error: %s", err)
 	}
