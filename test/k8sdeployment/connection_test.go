b340d5b85db57ec9afe106292798c7640f5ac018
Sandeep Rajan <srajan@infoblox.com>
2020-06-16 12:36:59 -0400

    fix skipped tests
    Signed-off-by: Sandeep Rajan <srajan@infoblox.com>

--- test/k8sdeployment/connection_test.go
+++ test/k8sdeployment/connection_test.go
@@ -1,6 +1,7 @@
 package k8sdeployment
 
 import (
+	"fmt"
 	"os/exec"
 	"testing"
 
@@ -11,10 +12,6 @@ import (
 // is valid and there are no failures.
 // This test is to catch bugs/errors such as the one reported in https://github.com/coredns/coredns/issues/2464
 func TestConnectionAfterAPIRestart(t *testing.T) {
-
-	t.Skip("Test needs to be refactored for kind environment")
-	return
-
 	// Apply manifests via coredns/deployment deployment script ...
 	cmd := exec.Command("sh", "-c", " ~/go/src/${CIRCLE_PROJECT_USERNAME}/deployment/kubernetes/deploy.sh -s -i 10.96.0.10 -r 10.96.0.0/8 -r 172.17.0.0/16 | kubectl apply -f -")
 	cmdout, err := cmd.CombinedOutput()
@@ -29,33 +26,49 @@ func TestConnectionAfterAPIRestart(t *testing.T) {
 	}
 
 	// Check if CoreDNS has restarted before the APIServer restart
-	restartCount, err := kubernetes.HasCoreDNSRestarted()
+	hasCoreDNSRestarted, err := kubernetes.HasResourceRestarted(kubernetes.CoreDNSLabel)
 	if err != nil {
 		t.Fatalf("error fetching CoreDNS pod restart count: %s", err)
 	}
-	if restartCount {
+	if hasCoreDNSRestarted {
 		t.Fatalf("error as CoreDNS has crashed: %s.", kubernetes.CorednsLogs())
 	}
 
 	// Restart the Kubernetes APIServer.
-	dockerCmd, err := exec.Command("sh", "-c", "docker restart $(docker ps --no-trunc | grep 'kube-apiserver' | awk '{ print $1; }') > /dev/null").CombinedOutput()
+	containerID, err := kubernetes.FetchDockerContainerID("kind-control-plane")
 	if err != nil {
-		t.Fatalf("docker container restart failed: %s\nerr: %s", string(dockerCmd), err)
+		t.Fatalf("docker container ID not found, err: %s", err)
 	}
+
+	dockerCmd := fmt.Sprintf("docker exec -i %s /bin/sh -c \"pkill kube-apiserver\"", containerID)
+	killAPIServer, err := exec.Command("sh", "-c", dockerCmd).CombinedOutput()
+	if err != nil {
+		t.Fatalf("API Server restart failed: %s\nerr: %s", string(killAPIServer), err)
+	}
+
 	// Verify that the CoreDNS pods are up and ready after the restart.
 	maxWait = 120
 	if kubernetes.WaitNReady(maxWait, 1) != nil {
 		t.Fatalf("coredns failed to start in %v seconds,\nlog: %v", maxWait, kubernetes.CorednsLogs())
 	}
 
+	// Check if the api-server was actually restarted
+	hasAPIRestarted, err := kubernetes.HasResourceRestarted(kubernetes.APIServerLabel)
+	if err != nil {
+		t.Fatalf("error fetching kube-apiserver pod restart count: %s", err)
+	}
+	if !hasAPIRestarted {
+		t.Fatalf("API Server restart failed")
+	}
+
 	// Check if CoreDNS has crashed after APIServer restart
-	restartCount, err = kubernetes.HasCoreDNSRestarted()
+	hasCoreDNSRestarted, err = kubernetes.HasResourceRestarted(kubernetes.CoreDNSLabel)
 	if err != nil {
 		t.Fatalf("error fetching CoreDNS pod restart count: %s", err)
 	}
 
 	// If CoreDNS crashes due to KubeAPIServer restart, Kubernetes will restart the CoreDNS containers.
-	if restartCount {
+	if hasCoreDNSRestarted {
 		t.Fatalf("failed as CoreDNS crashed due to KubeAPIServer restart.")
 	}
 }
