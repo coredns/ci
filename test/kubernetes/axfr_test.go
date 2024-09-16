b43bfaf6f8dff75f3ae37f94f970be96d17bf458
Dave McCormick <davidemccormick@gmail.com>
2024-09-16 16:11:08 +0100

    Add AXFR tests to kubernetai plugin. Share AXFR testing tools from the kubernetes plugin.
    Signed-off-by: Dave McCormick <d_mccormick@apple.com>

--- test/kubernetes/axfr_test.go
+++ test/kubernetes/axfr_test.go
@@ -3,7 +3,6 @@ package kubernetes
 import (
 	"bufio"
 	"fmt"
-	"regexp"
 	"strings"
 	"testing"
 
@@ -15,7 +14,7 @@ import (
 // load answers turns a text based dig response into a set of answers
 func loadAXFRAnswers(t *testing.T, results string) []dns.RR {
 	s := bufio.NewScanner(strings.NewReader(results))
-	answers, err := parseDigAXFR(s)
+	answers, err := ParseDigAXFR(s)
 	if err != nil {
 		t.Fatalf("failed to parse expected AXFR results: %v", err)
 		return []dns.RR{}
@@ -96,7 +95,10 @@ cluster.local.		5	IN	SOA	ns.dns.cluster.local. hostmaster.cluster.local. 1726438
 				t.Errorf(err.Error())
 			}
 			if res != nil {
-				validateAXFR(t, res.Answer, tc.Answer)
+				failures := ValidateAXFR(res.Answer, tc.Answer)
+				for _, flr := range failures {
+					t.Error(flr)
+				}
 			}
 			if t.Failed() {
 				t.Errorf("coredns log: %s", CorednsLogs())
@@ -162,124 +164,14 @@ cluster.local.		5	IN	SOA	ns.dns.cluster.local. hostmaster.cluster.local. 1726233
 				t.Errorf(err.Error())
 			}
 			if res != nil {
-				validateAXFR(t, res.Answer, tc.Answer)
+				failures := ValidateAXFR(res.Answer, tc.Answer)
+				for _, flr := range failures {
+					t.Error(flr)
+				}
 			}
 			if t.Failed() {
 				t.Errorf("coredns log: %s", CorednsLogs())
 			}
 		})
 	}
-}
-
-// validateAXFR compares the dns records returned against a set of expected records.
-// It ensures that the axfr response begins and ends with an SOA record.
-// It will only test the first 3 tuples of each A record.
-func validateAXFR(t *testing.T, xfr []dns.RR, expected []dns.RR) {
-	if xfr[0].Header().Rrtype != dns.TypeSOA {
-		t.Error("Invalid transfer response, does not start with SOA record")
-	}
-	if xfr[len(xfr)-1].Header().Rrtype != dns.TypeSOA {
-		t.Error("Invalid transfer response, does not end with SOA record")
-	}
-
-	// make a map of xfr responses to search...
-	xfrMap := make(map[int]dns.RR, len(xfr))
-	for i := range xfr {
-		xfrMap[i] = xfr[i]
-	}
-
-	// for each expected entry find a result response which matches.
-	for i := range expected {
-		matched := false
-		for key, resultRR := range xfrMap {
-			if !matchHeader(t, expected[i].Header(), resultRR.Header()) {
-				continue
-			}
-
-			// headers match
-			// special matchers and default full match
-			switch expected[i].Header().Rrtype {
-			case dns.TypeSOA, dns.TypeA:
-				matched = true
-				break
-			case dns.TypeSRV:
-				if matchSRVResponse(t, expected[i].(*dns.SRV), resultRR.(*dns.SRV)) {
-					matched = true
-				}
-				break
-			default:
-				if dns.IsDuplicate(expected[i], resultRR) {
-					matched = true
-				}
-			}
-
-			if matched {
-				delete(xfrMap, key)
-				break
-			}
-		}
-		if !matched {
-			t.Errorf("this AXFR record does not match any results:\n%s\n", expected[i])
-		}
-	}
-
-	if len(xfr) > len(expected) {
-		t.Errorf("Invalid number of responses, want %d, got %d", len(expected), len(xfr))
-	}
-}
-
-// matchHeader will return true when two headers are exactly equal or the expected and resultant header
-// both contain a dashed ip address and the domain matches.
-func matchHeader(t *testing.T, expected, result *dns.RR_Header) bool {
-	if expected.Rrtype != result.Rrtype {
-		return false
-	}
-	if expected.Class != result.Class {
-		return false
-	}
-	if expected.Rrtype != result.Rrtype {
-		return false
-	}
-	expectedNameReg, err := zoneToRelaxedRegex(expected.Name)
-	if err != nil {
-		t.Fatalf("failed to covert dns name %s to regex: %v", expected.Name, err)
-	}
-	if !expectedNameReg.MatchString(result.Name) {
-		return false
-	}
-	return true
-}
-
-// validateSRVResponse matches an SRV response record
-func matchSRVResponse(t *testing.T, expectedSRV, resultSRV *dns.SRV) bool {
-	expectedTargetReg, err := zoneToRelaxedRegex(expectedSRV.Target)
-	if err != nil {
-		t.Fatalf("failed to covert srv target %s to regex: %v", expectedSRV.Target, err)
-	}
-	if !expectedTargetReg.MatchString(resultSRV.Target) {
-		return false
-	}
-
-	// test other SRV record attributes...
-	if expectedSRV.Port != resultSRV.Port {
-		return false
-	}
-	if expectedSRV.Priority != resultSRV.Priority {
-		return false
-	}
-	if expectedSRV.Weight != resultSRV.Weight {
-		return false
-	}
-	return true
-}
-
-var ipPartMatcher = regexp.MustCompile(`^\d+-\d+-\d+-\d+\.`)
-
-// zoneToRelaxedRegex creates a regular expression from a domain name, replacing ipv4 dashed addresses with a
-// more generalised matcher that will match any address.
-func zoneToRelaxedRegex(source string) (*regexp.Regexp, error) {
-	if !ipPartMatcher.MatchString(source) {
-		return regexp.Compile(`^` + source + `$`)
-	}
-	return regexp.Compile(ipPartMatcher.ReplaceAllString(source, `^\d+-\d+-\d+-\d+\.`) + `$`)
 }
